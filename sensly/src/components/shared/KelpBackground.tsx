9404707405b80ee99fe1b74a24edf0913977970b
Ceya Badyal <cbadyal@calpoly.edu>
2026-05-02 20:10:09 -0700

    Use background_kelp.png as the kelp background image

--- sensly/src/components/shared/KelpBackground.tsx
+++ sensly/src/components/shared/KelpBackground.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 import { View, Image, StyleSheet, Dimensions } from 'react-native';
 
 // @ts-ignore
-import kelpBottom from '../../../assets/kelp_2.png';
+import kelpBg from '../../../assets/background_kelp.png';
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
 
@@ -20,7 +20,7 @@ export function KelpBackground({ children, opacity = 0.3 }: KelpBackgroundProps)
     <View style={styles.container}>
       {/* Full-screen kelp background image */}
       <Image
-        source={kelpBottom}
+        source={kelpBg}
         style={[styles.bgImage, { opacity }]}
         resizeMode="cover"
         pointerEvents="none"
